273b31e6219760a7cd6107fe3943ba3c26104d13
haseebfarrukh <sp19bscs0035@maju.edu.pk>
2019-04-21 17:41:14 +0500

    haseebfarrukh

---
+++ haseeb.py
@@ -0,0 +1,18 @@
+positive=0
+negative=0
+while True:
+    value=input("Enter an integer from -35 to 35: ")
+   
+    if (value=="x"):
+        break
+    else:
+        value=int(value)
+
+        if (value>0 and value<35):
+            positive+=1
+
+        elif (value < 0 and value > -35):
+            negative+=1
+
+print("Total Positive Numbers=",positive)
+print("Total Negative Numbers=",negative)
