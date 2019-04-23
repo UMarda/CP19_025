79abdf8db6e3c5d6b72d6633462573ee5518939d
sp0074 <sp19bscs007@maju.edu.pk>
2019-04-23 10:29:39 -0700

    sarah.py

--- sarah.py
+++ sarah.py
@@ -1,16 +1,12 @@
-a=int(input("range:"))
-positive=0
-negative=0
-for i in range(a):
-  y=input()
-  if(y=="x"):
-    print("the end")
-    break
-  else:
-    z=int(y)
-    if(z<0):
-      negative+=1
-    elif(z>=0):
-      positive+=1
-print("total positive numbers: ",positive,"\nTotal negative numbers: ",negative)
-    
+#money counting games
+p=float(input("enter the number of pennies="))
+n=float(input("enter the number of nickels="))
+d=float(input("enter the number of dimes="))
+q=float(input("enter the number of quarters="))
+sum=p+n+d+q
+if(sum==1):
+    print("congratulations! you win")
+elif(sum<=1):
+    print("the amount entered was less than 1$")
+else:
+    print("the amount entered was greater than 1$")
