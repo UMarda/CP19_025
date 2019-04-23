df16ac705ea617a32de8e92245020f16e7373ebe
sp0074 <49778929+sp0074@users.noreply.github.com>
2019-04-23 10:32:19 -0700

    Update sarah.py

--- sarah.py
+++ sarah.py
@@ -1,12 +1,12 @@
 #money counting games
-p=float(input("enter the number of pennies="))
-n=float(input("enter the number of nickels="))
-d=float(input("enter the number of dimes="))
-q=float(input("enter the number of quarters="))
+p=float(input("enter the number of pennies= "))
+n=float(input("enter the number of nickels= "))
+d=float(input("enter the number of dimes= "))
+q=float(input("enter the number of quarters= "))
 sum=p+n+d+q
 if(sum==1):
-    print("congratulations! you win")
+    print("Congratulations! you win")
 elif(sum<=1):
-    print("the amount entered was less than 1$")
+    print("The amount entered was less than 1$")
 else:
-    print("the amount entered was greater than 1$")
+    print("The amount entered was greater than 1$")
