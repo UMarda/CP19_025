e0474c56230c9dc1451b67470a844d2408f924b9
sp0074 <49778929+sp0074@users.noreply.github.com>
2019-04-24 01:58:29 -0700

    Update sarah.py

--- sarah.py
+++ sarah.py
@@ -7,8 +7,8 @@ while True:
         num=int(num)
         sr=int(math.sqrt(num))
         if num==sr**2:
-            print("The num ",num,"is a perfect square of ",sr)
+            print("the num ",num,"is a perfect square of ",sr)
         elif num!=sr**2:
-            print("The num ",num,"is not a perfect square ")
+            print("the num ",num,"is not a perfect square ")
     num=(input("Enter a number="))
     
