415ac7d7ddbcb383800839902ad653f86a1aa83a
ar0027 <49778706+ar0027@users.noreply.github.com>
2019-04-24 01:33:27 -0700

    Update areeba.py

--- areeba.py
+++ areeba.py
@@ -1,13 +1,9 @@
-#SHAPEOFTRIANGLE
-i=1
-while(i<8):
-    j=8
-    while(j>i+1):
-        print(" ",end=" ")
-        j=j-1
-    k=1
-    while(k<i*2):
-        print("*",end=" ")
-        k=k+1
-    i=i+1
-    print(" ")
+#MAGICDATE
+month=int(input("enter month="))
+day=int(input("enter day="))
+year=int(input("enter year"))
+magic=month*day
+if(magic==year):
+    print("the date is magic")
+else:
+    print("error")
