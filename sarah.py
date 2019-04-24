a09df1280059e0b1eb9704ed9038ea71d86c2541
sp0074 <49778929+sp0074@users.noreply.github.com>
2019-04-24 01:41:57 -0700

    Update sarah.py

--- sarah.py
+++ sarah.py
@@ -1,12 +1,14 @@
-#money counting games
-p=float(input("Enter the number of pennies= "))
-n=float(input("Enter the number of nickels= "))
-d=float(input("Enter the number of dimes= "))
-q=float(input("Enter the number of quarters= "))
-sum=p+n+d+q
-if(sum==1):
-    print("Congratulations! you win")
-elif(sum<=1):
-    print("The amount entered was less than 1$")
-else:
-    print("The amount entered was greater than 1$")
+import math
+num=input("Enter a number=")
+while True:
+    if num=="x":
+        break
+    else:
+        num=int(num)
+        sr=int(math.sqrt(num))
+        if num==sr**2:
+            print("The num",num,"is a perfect square of ",sr)
+        elif num!=sr**2:
+            print("The num",num,"is not a perfect square ")
+    num=(input("Enter a number="))
+    
