ae2239b844360af8cd752b3ab318cf7dfa502b36
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-25 20:21:43 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,13 +1,18 @@
-import datetime
-d=input("Enter:")
-r=d.split("/")
-date=datetime.datetime.now()
-e=int(r[0])
-f=int(r[1])
-g=int(r[2])
-if e>date.day or f>date.month or g>date.year:
-    print("Greater")
-elif e<date.day or f<date.month or g<date.year: 
-    print("Lesser")
-else:
-    print("Equal")
+def prime(value):
+    if(value>0):
+        i=1
+        count=0
+        result=0
+        while(i<=value):
+            result=value%i
+            
+            if(result==0):
+                count+=1
+            i+=1
+        if(count==2):
+            print(value," is a Prime Number")
+        
+        else:
+            print(value," Is Not Prime Number")
+value=int(input("Enter A Number : "))
+prime(value)
