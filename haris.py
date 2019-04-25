3d221cce47b2a87629a3ed6ae29d90b5abc969b6
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-25 20:52:14 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,8 +1,10 @@
-a=input("Enter a word:")
-print(a[:])
-print(a[::-1])
-if(a==a[::-1]):
-    print("It is pallendrome")
-else:
-    print("It is not pallendrome")
-    
+def triangle():
+  x=int(input("Enter number:"))
+  for i in range(1,6):
+    for j in range(i,7-1):
+        print(" ",end=" ")
+    for k in range(1,1*i):
+        print("5 9",end=" ")
+    print("5")
+triangle()
+  
