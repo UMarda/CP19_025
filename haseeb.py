fcaa29e466dfb788a894a2f3a1c386a41effea17
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-04-26 12:08:40 +0500

    Update haseeb.py

--- haseeb.py
+++ haseeb.py
@@ -1,12 +1,27 @@
-# Number Analyser 
+#Roman Numerals
 
-N=int(input("Please Enter a Number: "))
 
-if(N>0):
-  print("Positive Number")
-  if(N%2==0):
-    print("Even")
-  else:
-    print("Odd")
+no=int(input("Enter a Number from 1 To 10 : "))
+if(no==1):
+    print("I")
+elif(no==2):
+    print("II")
+elif(no==3):
+    print("III")
+elif(no==4):
+    print("IV")
+elif(no==5):
+    print("V")
+elif(no==6):
+    print("VI")
+elif(no==7):
+    print("VII")
+elif(no==8):
+    print("VIII")
+elif(no==9):
+    print("IX")
+elif(no==10):
+    print("X")
+  
 else:
-  print("Negative Number")
+    print("NUMBER ERROR")
