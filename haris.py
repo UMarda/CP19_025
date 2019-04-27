6d924cd4cca5280cd01732d0b7e9b4c6e5509ff9
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-27 11:56:52 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,12 +1,5 @@
-d=input("Enter a paragraph:")
-v=""
-d_c=True
-for j in d:
-    if j==".":
-        d_c=True
-    if j.isalpha() and d_c:
-        v+=j.upper()
-        d_c=False
-    else:
-        v+=j
-print(v)        
+import tkinter
+a=tkinter.Tk()
+a.title("Taking bot")
+a.geometry("640x460")
+a.mainloop()
