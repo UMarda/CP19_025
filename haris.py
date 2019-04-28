877ef18dd0ab3a6d3995dabb3c381ab4c93640ac
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-28 22:04:21 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,43 +1,16 @@
-def diamond():
-    g=int(input("Enter shape lenght:"))
-    h=int(input("Enter the first number:"))
-    x=int(input("Enter second number:"))
-    i=1
-    while(i<=g-1):
-        j=g-1
-        while(j>=i):
-            print(" ",end=' ')
-            j-=1
-        k=1
-        while(k<i*2):
-            if(k==i):
-                print(h,end=' ')
-            else:
-                print(x,end=' ')
-            k+=1
-    
-        i+=1
-        print("")
-    for z in range(1,(g*2)):
-        print(h,end=' ')
-    print("")
+def num():
+    num=int(input("Enter the number:"))
+    c=num
     a=1
-    while(a<=g-1):
-        b=2
-        while(b<=a+1):
-            print(" ",end=' ')
-            b+=1
-        c=g-1
-        while(c>=a):
-            if(c==a):
-                print(h,end=' ')
-            else:
-                print(x,end=' ')
-            c-=1
-            d=g-2
-        while(d>=a):
-            print(x,end=' ')
-            d-=1
-        a+=1
-        print()
-diamond()
+    newnum=0
+    while(num>0):
+        rem=num%10
+        num=num//10
+        newnum=newnum*10+(rem)
+    print("reverse number is",newnum)
+  
+    if(c==newnum):
+        print("reverse number is same")
+    else:
+        print("reverse number is not same")
+num()    
