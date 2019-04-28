c56c8408dae82a81a27be743fd5c5ba1a9c6e0b8
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-28 22:00:35 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,5 +1,10 @@
-import tkinter
-a=tkinter.Tk()
-a.title("Taking bot")
-a.geometry("640x460")
-a.mainloop()
+def triangle():
+  x=int(input("Enter number:"))
+  for i in range(1,6):
+    for j in range(i,7-1):
+        print(" ",end=" ")
+    for k in range(1,1*i):
+        print("5 9",end=" ")
+    print("5")
+triangle()
+  
