7387460a3f269d3a07640ebe060ec64d076f045a
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-28 22:08:08 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,16 +1,13 @@
-def num():
-    num=int(input("Enter the number:"))
-    c=num
-    a=1
-    newnum=0
-    while(num>0):
-        rem=num%10
-        num=num//10
-        newnum=newnum*10+(rem)
-    print("reverse number is",newnum)
-  
-    if(c==newnum):
-        print("reverse number is same")
-    else:
-        print("reverse number is not same")
-num()    
+import datetime
+d=input("Enter:")
+r=d.split("/")
+date=datetime.datetime.now()
+e=int(r[0])
+f=int(r[1])
+g=int(r[2])
+if e>date.day or f>date.month or g>date.year:
+    print("Greater")
+elif e<date.day or f<date.month or g<date.year: 
+    print("Lesser")
+else:
+    print("Equal")
