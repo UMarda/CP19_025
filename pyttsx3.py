6ade707e90665c171331873d2710f90f743405a2
sp0074 <sp19bscs007@maju.edu.pk>
2019-04-28 10:55:26 -0700

    text to speech

--- pyttsx3.py
+++ pyttsx3.py
@@ -2,11 +2,11 @@
 import pyttsx3
 y=input("enter a sentence: ")  #input sentence 1
 z=input("enter a sentence: ")  #input sentence 2
-while(y!="exit"):             
+while(y!="exit"):             #stop taking input 
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)    
     engine.setProperty('volume', 0.9) 
     engine.say(y)        #say sentence 1
     engine.say(z)        #say sentence 2
     engine.runAndWait()
-    y=input("enter a sentence: ")
+    y=input("enter a sentence: ") #again taking sentence
