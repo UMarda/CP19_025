5507cb65bce1ac2ed37fc0fb9d85d343492f7e7f
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-28 22:02:49 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,10 +1,43 @@
-def triangle():
-  x=int(input("Enter number:"))
-  for i in range(1,6):
-    for j in range(i,7-1):
-        print(" ",end=" ")
-    for k in range(1,1*i):
-        print("5 9",end=" ")
-    print("5")
-triangle()
-  
+def diamond():
+    g=int(input("Enter shape lenght:"))
+    h=int(input("Enter the first number:"))
+    x=int(input("Enter second number:"))
+    i=1
+    while(i<=g-1):
+        j=g-1
+        while(j>=i):
+            print(" ",end=' ')
+            j-=1
+        k=1
+        while(k<i*2):
+            if(k==i):
+                print(h,end=' ')
+            else:
+                print(x,end=' ')
+            k+=1
+    
+        i+=1
+        print("")
+    for z in range(1,(g*2)):
+        print(h,end=' ')
+    print("")
+    a=1
+    while(a<=g-1):
+        b=2
+        while(b<=a+1):
+            print(" ",end=' ')
+            b+=1
+        c=g-1
+        while(c>=a):
+            if(c==a):
+                print(h,end=' ')
+            else:
+                print(x,end=' ')
+            c-=1
+            d=g-2
+        while(d>=a):
+            print(x,end=' ')
+            d-=1
+        a+=1
+        print()
+diamond()
