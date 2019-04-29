afc5460ff6eaafb9ff2f52832301934ea803b800
ar0027 <49778706+ar0027@users.noreply.github.com>
2019-04-29 08:50:06 -0700

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -9,7 +9,7 @@ def clickf():
 root = tk.Tk()
 root.title("testing program")
 root.geometry("640x480")
-b1=tk.Button(root,text="click",command=clickf)
+b1=tk.Button(root,text="click",command=clickf,bg="blue")
 b1.pack()
 
 root.mainloop()
