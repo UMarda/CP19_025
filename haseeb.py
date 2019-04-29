f5f50e96d91e25beab380bfd67b65c212b70a68f
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-04-29 10:23:11 +0500

    Update haseeb.py

--- haseeb.py
+++ haseeb.py
@@ -1,28 +1,18 @@
-#Grade Calculator
+#Roulette Wheel Colors
 
-T1=int(input("Enter a Test 1 points: "))
-T2=int(input("Enter a Test 2 points: "))
-Exam=int(input("Enter a Main exam points: "))
-t=T1+T2+Exam
+Pocket = int (input("Enter pocket no from (0-36): "))
 
-if(T1<0 or T1>25 or T2<0 or T2>25 or exam<0 or Exam>50):
-     print("Error")
-        
-elif(t<50):
-    print("Fail")
-elif(exam<25):
-    print("Fail")
-
-elif(t>80):
-    print("Distinction")
-
-elif(t>60 and t<80):
-    print("Credit")
-
-elif(t<=60):
-     print("Pass")
-
-elif(t<60 and t>=50):
-     print("Pass")
-esle:
-    print("End")
+if (Pocket < 0 or Pocket > 36):
+    print("Error: Pocket no is out of range (0-36),")
+elif (Pocket == 0):
+    print ("Pocket color: GREEN")
+elif ( (Pocket >= 1 and Pocket <= 10) or (Pocket >= 19 and Pocket <= 28)):
+    if (Pocket%2 == 0):
+        print ("Pocket color: RED")
+    else:
+        print ("Pocket color: BLACK")
+else:
+    if (Pocket%2 != 0):
+        print ("Pocket color: RED")
+    else:
+        print ("Pocket color: BLACK")
