4b00e9378560fd9d090957033141b4b374509bf0
sp0074 <sp19bscs007@maju.edu.pk>
2019-04-29 11:25:05 -0700

    text to speech

--- pyttsx3.py
+++ pyttsx3.py
@@ -20,3 +20,12 @@ for voice in voices:
    engine.setProperty('voice', voice.id)
    engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
+
+#to input sentence for voice changing
+import pyttsx3
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+for voice in voices:
+   engine.setProperty('voice', voice.id)
+   engine.say('The quick brown fox jumped over the lazy dog.')
+engine.runAndWait()
