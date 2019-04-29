d27be04f9a5ad09cc047b012f7f82af1a69566c4
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-04-29 22:20:22 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,11 +1,18 @@
-n=input("Enter a string:")
-a=n.split(",")
-maxx=0
-minn=99999999
-for k in a:
-    if int(k)>maxx:
-        maxx=int(k)
-    if int(k)<minn:
-        minn=int(k)
-print("Maximum value in this list is",maxx)
-print("Minimum value in this list is",minn)
+def prime(value):
+    if(value>0):
+        i=1
+        count=0
+        result=0
+        while(i<=value):
+            result=value%i
+            
+            if(result==0):
+                count+=1
+            i+=1
+        if(count==2):
+            print(value," is a Prime Number")
+        
+        else:
+            print(value," Is Not Prime Number")
+value=int(input("Enter A Number : "))
+prime(value)
