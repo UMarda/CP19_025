9bdad12b2e63ef06943fd50886600fc2ddc00a88
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-04-29 10:21:14 +0500

    Update haseeb.py

--- haseeb.py
+++ haseeb.py
@@ -1,27 +1,28 @@
-#Roman Numerals
+#Grade Calculator
 
+T1=int(input("Enter a Test 1 points: "))
+T2=int(input("Enter a Test 2 points: "))
+Exam=int(input("Enter a Main exam points: "))
+t=T1+T2+Exam
 
-no=int(input("Enter a Number from 1 To 10 : "))
-if(no==1):
-    print("I")
-elif(no==2):
-    print("II")
-elif(no==3):
-    print("III")
-elif(no==4):
-    print("IV")
-elif(no==5):
-    print("V")
-elif(no==6):
-    print("VI")
-elif(no==7):
-    print("VII")
-elif(no==8):
-    print("VIII")
-elif(no==9):
-    print("IX")
-elif(no==10):
-    print("X")
-  
-else:
-    print("NUMBER ERROR")
+if(T1<0 or T1>25 or T2<0 or T2>25 or exam<0 or Exam>50):
+     print("Error")
+        
+elif(t<50):
+    print("Fail")
+elif(exam<25):
+    print("Fail")
+
+elif(t>80):
+    print("Distinction")
+
+elif(t>60 and t<80):
+    print("Credit")
+
+elif(t<=60):
+     print("Pass")
+
+elif(t<60 and t>=50):
+     print("Pass")
+esle:
+    print("End")
