06859c75bd2aeccec0b5b0168a0d3a2b70b4726e
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-04-30 23:58:16 +0500

    Update speech_recognition 2.py

--- speech_recognition 2.py
+++ speech_recognition 2.py
@@ -1,18 +1,12 @@
-import speech_recognition as sr
+import speech_recognition as sr     # import the library
 
-# Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+r = sr.Recognizer() # initialize recognizer
 
-# Speech recognition using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+with sr.Microphone() as source:     # mention source it will be either Microphone or audio files.
+    print("Speak Anything :")
+    audio = r.listen(source)        # listen to the source
+    try:
+        text = r.recognize_google(audio)    
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize your voice")   
