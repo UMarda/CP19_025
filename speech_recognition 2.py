b4f331175597f5bdb2c33860e1167f4cfec032e3
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-05-01 23:45:15 +0500

    Update speech_recognition 2.py

--- speech_recognition 2.py
+++ speech_recognition 2.py
@@ -2,13 +2,15 @@ import speech_recognition as sr
 
 # get audio from the microphone                                                                       
 r = sr.Recognizer()                                                                                   
-with sr.Microphone() as source:                                                                       
+with sr.Microphone() as source:   
+    
     print("Speak:")                                                                                   
     audio = r.listen(source)   
 
 try:
     print("You said " + r.recognize_google(audio))
+
 except sr.UnknownValueError:
-    print("Could not understand audio")
+    print("Could not understand your audio")
 except sr.RequestError as e:
     print("Could not request results; {0}".format(e))
