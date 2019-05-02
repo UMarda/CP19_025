a9b137400f40e4081123f568f256236831639cd0
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-05-02 23:19:38 +0500

    Update speech_recognition 2.py

--- speech_recognition 2.py
+++ speech_recognition 2.py
@@ -1,8 +1,9 @@
 import speech_recognition as sr
+
 recording = sr.Recognizer()
 
 with sr.Microphone() as source: recording.adjust_for_ambient_noise(source)
-    print("Please Say something:")
+    print("Kindly please say something :")
     audio = recording.listen(source)
     try:
         print("You said: \n" + recording.recognize_google(audio))
