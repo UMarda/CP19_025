ed76228939502ff2d76d9ec53e47023e83de0678
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-05-03 21:11:02 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,18 +1,12 @@
-def prime(value):
-    if(value>0):
-        i=1
-        count=0
-        result=0
-        while(i<=value):
-            result=value%i
-            
-            if(result==0):
-                count+=1
-            i+=1
-        if(count==2):
-            print(value," is a Prime Number")
-        
-        else:
-            print(value," Is Not Prime Number")
-value=int(input("Enter A Number : "))
-prime(value)
+d=input("Enter a paragraph:")
+v=""
+d_c=True
+for j in d:
+    if j==".":
+        d_c=True
+    if j.isalpha() and d_c:
+        v+=j.upper()
+        d_c=False
+    else:
+        v+=j
+print(v)        
