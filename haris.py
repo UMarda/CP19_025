32afc381d024cc85aac0d33ab54b5ec4cbb597e1
Hariskhan0059 <49777367+Hariskhan0059@users.noreply.github.com>
2019-05-04 23:36:27 +0500

    Update haris.py

--- haris.py
+++ haris.py
@@ -1,12 +1,13 @@
-d=input("Enter a paragraph:")
-v=""
-d_c=True
-for j in d:
-    if j==".":
-        d_c=True
-    if j.isalpha() and d_c:
-        v+=j.upper()
-        d_c=False
-    else:
-        v+=j
-print(v)        
+import datetime
+d=input("Enter:")
+r=d.split("/")
+date=datetime.datetime.now()
+e=int(r[0])
+f=int(r[1])
+g=int(r[2])
+if e>date.day or f>date.month or g>date.year:
+    print("Greater")
+elif e<date.day or f<date.month or g<date.year: 
+    print("Lesser")
+else:
+    print("Equal")
