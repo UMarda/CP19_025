996391165f92384f8994de4555351580896512d9
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-05-04 23:31:31 +0500

    Update speech_recognition 2.py

--- speech_recognition 2.py
+++ speech_recognition 2.py
@@ -6,6 +6,6 @@ with sr.Microphone() as source: recording.adjust_for_ambient_noise(source)
     print("Kindly please say something :")
     audio = recording.listen(source)
     try:
-        print("You said: \n" + recording.recognize_google(audio))
+        print("You have said: \n" + recording.recognize_google(audio))
 except Exception as e:
         print(e)
