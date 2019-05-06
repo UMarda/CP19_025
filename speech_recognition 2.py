272604d1bb95a71f1fb36aa6d2363918b48121bd
haseebfarrukh <49777384+haseebfarrukh@users.noreply.github.com>
2019-05-06 23:47:55 +0500

    Update speech_recognition 2.py

--- speech_recognition 2.py
+++ speech_recognition 2.py
@@ -1,11 +1,28 @@
-import speech_recognition as sr
+import speech_recognition as sr 
+mic_name = "USB Device 0x46d:0x825: Audio (hw:1, 0)"
+sample_rate = 48000
 
-recording = sr.Recognizer()
+chunk_size = 2048
+r = sr.Recognizer() 
 
-with sr.Microphone() as source: recording.adjust_for_ambient_noise(source)
-    print("Kindly please say something :")
-    audio = recording.listen(source)
-    try:
-        print("You have said: \n" + recording.recognize_google(audio))
-except Exception as e:
-        print(e)
+mic_list = sr.Microphone.list_microphone_names() 
+   
+    if microphone_name == mic_name: 
+        device_id = i 
+
+with sr.Microphone(device_index = device_id, sample_rate = sample_rate,  
+                        chunk_size = chunk_size) as source: 
+    r.adjust_for_ambient_noise(source) 
+    print "Say Something"
+    audio = r.listen(source) 
+          
+    try: 
+        text = r.recognize_google(audio) 
+        print "you said: " + text 
+      
+    except sr.UnknownValueError: 
+        print("Google Speech Recognition could not understand audio") 
+      
+    except sr.RequestError as e: 
+        print("Could not request results from Google  
+                                 Speech Recognition service; {0}".format(e))
