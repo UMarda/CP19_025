3f2c02de5b99b49b5f8a13ff08d7f5497372e0fd
ar0027 <49778706+ar0027@users.noreply.github.com>
2019-05-22 11:36:01 -0700

    Update GUI Coding.py

--- GUI Coding.py
+++ GUI Coding.py
@@ -15,19 +15,33 @@ def f2():
 
     mb.showinfo('second text','incorrect')
 root=tk.Tk()
+
 form=Frame()
 form.grid()
+
 root.title('Talking Bot')
+
 root.geometry('500x350')
+
 root.resizable(width=False, height=False)
+
 root.configure(bg='light blue')
+
 b1=tk.Button(root,text='enter text',command=f1,fg='red',bg='purple',height='2',width='10')
+
 b1.grid(row=0,column=1, padx=20, pady=50)
+
 b2=tk.Button(root,text='submit text',fg='blue',bg='pink',height='2',width='10')
+
 b2.grid(row=1,column=1, padx=20, pady=50)
+
 txt1 = scrolledtext.ScrolledText(root,width=30,height=1,bg='grey',fg='purple')
+
 txt1.grid(column=0,row=0)
+
 txt2 = scrolledtext.ScrolledText(root,width=30,height=1,bg='grey',fg='pink')
+
 txt2.grid(column=0,row=1)
+
 
 root.mainloop()
