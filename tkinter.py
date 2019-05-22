0ab51e35e2fc0437ea270ed00b6b101c464d6fab
ar0027 <49778706+ar0027@users.noreply.github.com>
2019-05-22 11:38:06 -0700

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -1,14 +1,23 @@
 #examplecodingsofGUI.
+
 import  tkinter as tk
+
 from tkinter import messagebox as mb
 
 def clickf():
+    
     print("Button clicked ")
+    
     mb.showinfo("My name","My message")
+    
 root = tk.Tk()
+
 root.title("testing program")
+
 root.geometry("640x480")
+
 b1=tk.Button(root,text="click",command=clickf,bg="blue")
+
 b1.pack()
 
 
